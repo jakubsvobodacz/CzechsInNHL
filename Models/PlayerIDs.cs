1b9c33670f122f602f274e8a4a218fc593ebdac1
JBSB (Jakub Svoboda) <JBSB@nnit.com>
2018-10-17 15:15:54 +0200

    added logo loading

--- Models/PlayerIDs.cs
+++ Models/PlayerIDs.cs
@@ -12,7 +12,26 @@ namespace CzechsInNHL.Models
             {"David Krejci", 8471276},
             {"Jakub Voracek", 8474161},
             {"Radko Gudas", 8475462},
-            {"Ondrej Palat", 8476292}
+            {"Ondrej Palat", 8476292},
+            {"Michael Frolik", 8473564},
+            {"Radek Faksa", 8476889},
+            {"Martin Frk", 8476924},
+            {"Martin Hanzal", 8471691},
+            {"Tomas Hertl", 8476881},
+            {"Dmitrij Jaskin", 8476436},
+            {"David Kampf", 8480144},
+            {"Ondrej Kase", 8478131},
+            {"Michal Kempny", 8479482},
+            {"Martin Necas", 8480039},                                                                    
+            {"Tomas Nosek", 8477931},
+            {"Tomas Plekanec", 8469521},
+            {"Roman Polak", 8471392},
+            {"Jan Rutta", 8480172},
+            {"Dominik Simon", 8478866},
+            {"Vladimir Sobotka", 8471743},
+            {"Libor Sulak", 8480161},
+            {"Jakub Vrana", 8477944},
+            {"Pavel Zacha", 8478401}
         };
     }
 }
